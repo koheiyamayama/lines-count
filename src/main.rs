c6eb149b99c8c2cfb220d5e6a76bde6ff8e0972c
koheiyamayama <kohei.ccw.23@gmail.com>
2021-06-19 23:43:21 +0900

    ファイルを読み込む

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
+use std::fs::File;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("#{:?}", args);
+    let path = &args[1];
+    let file = match File::open(path) {
+        Ok(file) => file,
+        Err(e) => panic!("couldn't open {}: {}", path, e.to_string()),
+    };
+    println!("{:?}", file);
 }
