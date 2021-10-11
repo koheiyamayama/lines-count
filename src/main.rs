50b03e5f6f786e4284187b69b68f36f85ccef3c3
koheiyamayama <kohei.ccw.23@gmail.com>
2021-10-11 23:25:43 +0900

    pathをPath::new使って表現する

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 use std::env;
-use std::fs;
+use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
-    let file = match fs::File::open(path) {
+    let path = Path::new(path);
+    let file = match File::open(path) {
         Ok(file) => file,
-        Err(e) => panic!("couldn't open {}: {}", path, e.to_string()),
+        Err(e) => {
+            println!("e: {}", e);
+            panic!("couldn't open {:?}: {}", path, e.to_string());
+        }
     };
+    println!("aaaa");
 
+    println!("{:?}", file);
     let file = BufReader::new(file);
+    println!("{:?}", file);
 
     let count = file.lines().count();
     println!("{}", count);
