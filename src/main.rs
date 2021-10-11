7a319cf03cad3265b7de7b13f19cdc8f60276bd0
koheiyamayama <kohei.ccw.23@gmail.com>
2021-10-12 00:21:04 +0900

    ディレクトリを指定した場合にディレクトリ内のエントリーを出力する

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
@@ -7,19 +7,29 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
     let path = Path::new(path);
-    let file = match File::open(path) {
-        Ok(file) => file,
-        Err(e) => {
-            println!("e: {}", e);
-            panic!("couldn't open {:?}: {}", path, e.to_string());
+    if path.is_file() {
+        let file = match File::open(path) {
+            Ok(file) => file,
+            Err(e) => {
+                println!("e: {}", e);
+                panic!("couldn't open {:?}: {}", path, e.to_string());
+            }
+        };
+        println!("aaaa");
+
+        println!("{:?}", file);
+        let file = BufReader::new(file);
+        println!("{:?}", file);
+
+        let count = file.lines().count();
+        println!("{}", count);
+    } else if path.is_dir() {
+        let entries = match fs::read_dir(path) {
+            Ok(dir) => dir,
+            Err(e) => panic!("error: {:?}", e),
+        };
+        for entry in entries {
+            println!("{:?}", entry);
         }
-    };
-    println!("aaaa");
-
-    println!("{:?}", file);
-    let file = BufReader::new(file);
-    println!("{:?}", file);
-
-    let count = file.lines().count();
-    println!("{}", count);
+    }
 }
