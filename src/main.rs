f5bb3218deb9e7fd8174f9f6b9f0fa648d3bd23d
koheiyamayama <kohei.ccw.23@gmail.com>
2021-10-12 00:45:40 +0900

    再帰的にファイルの行数を出力できるようにする

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,45 @@ fn main() {
     let path = &args[1];
     let path = Path::new(path);
     if path.is_file() {
-        let file = match File::open(path) {
-            Ok(file) => file,
-            Err(e) => {
-                println!("e: {}", e);
-                panic!("couldn't open {:?}: {}", path, e.to_string());
-            }
-        };
-        println!("aaaa");
-
-        println!("{:?}", file);
-        let file = BufReader::new(file);
-        println!("{:?}", file);
-
-        let count = file.lines().count();
+        let count = lines_count_file(path);
         println!("{}", count);
     } else if path.is_dir() {
-        let entries = match fs::read_dir(path) {
-            Ok(dir) => dir,
-            Err(e) => panic!("error: {:?}", e),
+        find_entries(path);
+    }
+}
+
+fn lines_count_file(path: &Path) -> usize {
+    let file = match File::open(path) {
+        Ok(file) => file,
+        Err(e) => {
+            panic!("couldn't open {:?}: {}", path, e.to_string());
+        }
+    };
+    let file = BufReader::new(file);
+    let count = file.lines().count();
+    count
+}
+
+fn find_entries(path: &Path) {
+    let entries = match fs::read_dir(path) {
+        Ok(dir) => dir,
+        Err(error) => panic!("error: {:?}", error),
+    };
+    for entry in entries {
+        let entry = match entry {
+            Ok(e) => e,
+            Err(error) => panic!("error: {:?}", error),
         };
-        for entry in entries {
-            println!("{:?}", entry);
+        let path = entry.path();
+        if path.is_file() {
+            let count = lines_count_file(&path);
+            let path = match path.as_path().to_str() {
+                Some(path) => path,
+                None => panic!("can't parse path."),
+            };
+            println!("{}: {}", path, count);
+        } else if path.is_dir() {
+            find_entries(&path);
         }
     }
 }
